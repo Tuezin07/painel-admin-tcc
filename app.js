93da0f2eeafb31d09a622dccfee7a50a1b7f0f76
Tuezin <138175938+Tuezin07@users.noreply.github.com>
2025-09-03 20:51:27 -0300

    Mudanças no código javascript

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Página de login
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
